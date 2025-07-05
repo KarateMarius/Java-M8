12f2d66fe7252fa35e706887091fa435c45ec5b1
KarateMarius <98535047+KarateMarius@users.noreply.github.com>
2025-07-05 19:29:51 +0200

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        for(int i = 0; i < 101; i++){
+
+            counter += i;
+
+        }
+        System.out.println(counter);
+    }
+}
