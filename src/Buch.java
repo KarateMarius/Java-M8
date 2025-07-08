e73dfd2cdca1c9fe80b0eb231d627fead0bf3e8b
KarateMarius <brodaufmarius@gmail.com>
2025-07-08 11:14:17 +0200

    Buch bis Folie 76

---
+++ src/Buch.java
@@ -0,0 +1,95 @@
+public class Buch {
+
+    private String autor;
+    private String titel;
+    private double preis;
+    private int auflage;
+
+    public void gibInfo(){
+
+    System.out.println("Der Autor heißt: " + autor);
+    System.out.println("Der Titel lautet: " + titel);
+    System.out.println("Es kostet: " + preis + "€");
+    System.out.println("Es ist die " + auflage + ". Auflage");
+
+    }
+
+    public String gibAutor(){
+
+        return autor;
+
+    }
+
+    public String gibTitel(){
+
+        return titel;
+    }
+
+    public double gibPreis(){
+
+        return preis;
+    }
+
+    public int gibAuflage(){
+
+        return auflage;
+    }
+
+    public double berechneMwSt(double buchpreis){
+
+        double mwst = 1.07;
+        buchpreis = buchpreis * mwst;
+
+        return buchpreis;
+    }
+
+    public void setAuflage(int auflage){
+
+        this.auflage = auflage;
+    }
+
+    public void setAutor(String autor){
+
+        this.autor = autor;
+
+    }
+
+    public void setTitel(String titel){
+
+        this.titel = titel;
+    }
+
+
+    public void setPreis(double preis){
+
+        this.preis = preis;
+    }
+}
+
+
+class Buchhaendler {
+
+    public static void main (String[] args){
+
+        Buch buch1 = new Buch();
+
+        buch1.gibInfo();
+        System.out.println(buch1.gibAutor());
+        System.out.println(buch1.gibTitel());
+        System.out.println(buch1.gibPreis());
+        System.out.println(buch1.gibAuflage());
+        System.out.println(buch1.berechneMwSt(buch1.gibPreis()));
+
+        buch1.setAutor("Marius");
+        buch1.setTitel("Was geht hier ab");
+        buch1.setPreis(25);
+        buch1.setAuflage(3);
+
+        buch1.gibInfo();
+        System.out.println("Der Preis mit MwSt ist: " + buch1.berechneMwSt(buch1.gibPreis()) + "€");
+
+    }
+
+}
+
+
