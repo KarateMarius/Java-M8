b3d979f55f7077206fd3b09824bbaaa673e1ab31
KarateMarius <brodaufmarius@gmail.com>
2025-07-09 11:40:13 +0200

    Teil 4 Folie 108

--- src/Buch.java
+++ src/Buch.java
@@ -6,14 +6,14 @@ public class Buch {
     private String titel;
     private double preis;
     private int auflage;
-
+    private Scanner sc = new Scanner(System.in);
     public void gibInfo(){
 
     System.out.println("Der Autor heißt: " + autor);
     System.out.println("Der Titel lautet: " + titel);
     System.out.println("Es kostet: " + preis + "€");
     System.out.println("Es ist die " + auflage + ". Auflage");
-    System.out.println("Die enthaltene MwSt beträgt: " + berechneMwSt(gibPreis()) + "€");
+    System.out.printf("Die enthaltene MwSt beträgt: %.2f€\n", berechneMwSt(gibPreis()));
     }
 
     public String gibAutor(){
@@ -66,6 +66,30 @@ public class Buch {
         if (preis < 0) preis = 0;
         this.preis = preis;
     }
+
+    public void datenEingabe(){
+
+        System.out.print("Bitte den Namen des Autors eingeben: ");
+        setAutor(sc.nextLine());
+        System.out.print("Bitte Titel des Buches eingeben: ");
+        setTitel(sc.nextLine());
+        System.out.print("Bitte den Preis eingeben: ");
+        setPreis(sc.nextDouble());
+        System.out.print("Bitte die Auflage eingeben: ");
+        setAuflage(sc.nextInt());
+
+    }
+
+    public Buch(String autor, String titel, double preis,  int auflage){
+
+        this.autor = autor;
+        this.titel = titel;
+        this.preis = preis;
+        this.auflage = auflage;
+    }
+
+
+
 }
 
 
@@ -75,16 +99,7 @@ class Buchhaendler {
 
         Scanner scanner = new Scanner(System.in);
 
-        Buch buch1 = new Buch();
-        System.out.print("Bitte den Namen des Autors eingeben: ");
-        buch1.setAutor(scanner.nextLine());
-        System.out.print("Bitte Titel des Buches eingeben: ");
-        buch1.setTitel(scanner.nextLine());
-        System.out.print("Bitte den Preis eingeben: ");
-        buch1.setPreis(scanner.nextDouble());
-        System.out.print("Bitte die Auflage eingeben: ");
-        buch1.setAuflage(scanner.nextInt());
-
+        Buch buch1 = new Buch("Marius", "VIT", 23, 23);
         buch1.gibInfo();
 
 
